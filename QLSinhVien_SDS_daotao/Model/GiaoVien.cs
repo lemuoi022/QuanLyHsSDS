b909f76585b2f11a4ec9ccfdb7400f3b9ed1adb3
Lê Xuân Mười <muoi0886@gmail.com>
2024-05-28 17:10:21 +0700

    a

--- QLSinhVien_SDS_daotao/Model/GiaoVien.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace QLSinhVien_SDS_daotao.Model
-{
-    internal class GiaoVien
-    {
-        public virtual int ID { get; set; }
-        public virtual string Ten { get; set; }
-        public virtual DateTime NgaySinh { get; set; }    
-    }
-}
