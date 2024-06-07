cc5dd101f6e80e19cfa1beb6eb69fb179af22c75
Lê Xuân Mười <muoi0886@gmail.com>
2024-06-07 14:52:58 +0700

    update

--- SQL_SinhVien.sql
+++ SQL_SinhVien.sql
@@ -2,9 +2,9 @@ use SINHVIEN
 
 CREATE TABLE SinhVien  (
     id  INT IDENTITY(1,1) PRIMARY KEY ,
-    ten varchar(255) not null,
+    ten nvarchar(255) not null,
     ngaysinh date not null,
-	diachi varchar(255) not null,
+	diachi nvarchar(255) not null,
 	id_lophoc int not null,
 	CONSTRAINT fk_sv_id_lophoc
   FOREIGN KEY (id_lophoc)
@@ -14,7 +14,7 @@ CREATE TABLE SinhVien  (
 CREATE TABLE LopHoc  (
     id  INT IDENTITY(1,1) PRIMARY KEY ,
     tenlop varchar(255) not null,
-	monhoc varchar(255) not null,
+	monhoc nvarchar(255) not null,
 	id_giaovien int not null
 	CONSTRAINT fk_lh_id_giaovien
   FOREIGN KEY (id_giaovien)
@@ -23,7 +23,7 @@ CREATE TABLE LopHoc  (
 
 CREATE TABLE GiaoVien  (
     id  INT IDENTITY(1,1) PRIMARY KEY ,
-    ten varchar(255) not null,
+    ten nvarchar(255) not null,
 	ngaysinh date not null,
 	
 );
