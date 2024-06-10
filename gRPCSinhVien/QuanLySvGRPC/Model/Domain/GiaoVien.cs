792c35c540449a653a12014f7a854c7cd75e2725
Lê Xuân Mười <muoi0886@gmail.com>
2024-06-10 15:35:35 +0700

    push

--- gRPCSinhVien/QuanLySvGRPC/Model/Domain/GiaoVien.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace QuanLySvGRPC.Model.Domain
-{
-    public class GiaoVien
-    {
-        public virtual int ID { get; set; }
-        public virtual string Ten { get; set; }
-        public virtual DateTime NgaySinh { get; set; }
-    }
-}
